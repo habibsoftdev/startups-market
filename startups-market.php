f86363bcd7dc4fabc0923adefc51b27dc82be1ab
Habibur Rahman <softdev.habib@gmail.com>
2023-12-10 20:41:07 +0600

    initial commit

---
+++ startups-market.php
@@ -0,0 +1,20 @@
+<?php 
+/*
+Plugin Name: Startups Market 
+Plugin URI: https://giantwpsolutions.com
+Description: Empower your entrepreneurial journey with Startups Market – the ultimate platform for buying and selling innovative startups. Connect with visionary founders, explore cutting-edge businesses, and facilitate secure transactions. Monetize your success with a streamlined certain percentage of transaction fee. Build the future of business, one startup at a time.
+Version: 1.0
+Author: Habib Rahman
+Author URI: https://habibr.me 
+License: GPLv2 or later
+Text Domain: startups-market
+Domain Path: /languages/
+*/
+
+/**
+ * Direct access not allowed
+ */
+
+if( ! Defined( 'ABSPATH' )){
+    exit;
+}
